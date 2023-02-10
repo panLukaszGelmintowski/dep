41adc6ff3974ea3ba738b7069a3be0ff3c664d0e
panLukaszGelmintowski <vsevolod.doronin@yandex.ru>
2023-02-10 15:44:40 +0500

    fix

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Тестовая БД
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
